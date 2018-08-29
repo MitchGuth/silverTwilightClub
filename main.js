e8bdfe60a13eee21bd4c836b3f7f55e89db69524
Nat Sharpe <sharpe1890@gmail.com>
2018-08-29 15:24:30 -0400

    makes create account page and universal enter page for verified members

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 let urlAPI = "http://127.0.0.1:5000/";
 let token = localStorage.getItem('silvertwilight');
 
+let firstLoginButton = document.querySelector('.login-btn');
+let accountButtton = document.querySelector('.account-btn');
+let submitLoginButton = document.querySelector('.submit-login-btn');
+let submitAccountButton = document.querySelector('.submit-account-btn');
+// Select pages to hide or show
+let landingPage = document.querySelector('.landing-page');
+let loginPage = document.querySelector('.login-page');
+let accountPage = document.querySelector('.account-page');
+let enterPage = document.querySelector('.enter-page');
 
 let login = loginInfo => {
+    console.log('working')
     postPromise = fetch(`${urlAPI}login`, 
         {
             method: "post",
@@ -22,38 +32,82 @@ let login = loginInfo => {
             } else {
                 console.log('Storing Your Token...');
                 localStorage.setItem('silvertwilight', text);
+                loginPage.classList.toggle('hidden');
+                enterPage.classList.toggle('hidden');
             }
         });
     });
 };
 
-let clickLogin = event => {
+let createAccount = accountInfo => {
+    postPromise = fetch(`${urlAPI}account`, 
+        {
+            method: "post",
+            // mode: "no-cors",
+            headers: {'content-type':'application/json'},
+            body: JSON.stringify(accountInfo)
+        });
+    postPromise.catch(e => {
+        console.log(e.message);
+    });
+    // console.log(postPromise);
+    postPromise.then(function(response) {
+        response.text().then(function(text) {
+            if (text === "ACCOUNT CREATION FAIL") {
+                // do something in the DOM and say "You suck."
+                console.log("bok bok bok");
+            } else {
+                console.log('Storing Your Token...');
+                localStorage.setItem('silvertwilight', text);
+                accountPage.classList.toggle('hidden');
+                enterPage.classList.toggle('hidden');
+            }
+        });
+    });
+};
+
+let clickSubmitLogin = event => {
     event.preventDefault();
-    let emailAddress = document.querySelector('[name="emailAddress"]');
+    let email = document.querySelector('[name="email"]');
     let password = document.querySelector('[name="password"]');
     let loginInfo = {
-        'email': emailAddress.value,
+        'email': email.value,
         'password': password.value
     };
     login(loginInfo);
 };
 
 let clickFirstLogin = event => {
-    let landingPage = document.querySelector('.landing-page');
-    let loginPage = document.querySelector('.login-page');
     event.preventDefault();
     if (token) {
-        // run getStats function
+        enterPage.classList.toggle('hidden');
     } else {
         landingPage.classList.toggle('hidden');
         loginPage.classList.toggle('hidden');
     }
 };
 
-let firstLoginButtton = document.querySelector('.login-btn');
-let accountButtton = document.querySelector('.account-btn');
-let loginButtton = document.querySelector('.submit-btn');
+let clickSubmitAccount = event => {
+    event.preventDefault();
+    let email = document.querySelector('[name="account-email"]');
+    let password = document.querySelector('[name="account-password"]');
+    let name = document.querySelector('[name="account-name"]');
+    let accountInfo = {
+        'account-email': email.value,
+        'account-password': password.value,
+        'account-name': name.value
+    };
+    createAccount(accountInfo);
+};
 
-firstLoginButtton.addEventListener('click', clickFirstLogin);
-// accountButtton.addEventListener('click', clickAccount);
-loginButtton.addEventListener('click', clickLogin);
+let clickAccount = event => {
+    event.preventDefault();
+    landingPage.classList.toggle('hidden');
+    accountPage.classList.toggle('hidden');
+};
+
+firstLoginButton.addEventListener('click', clickFirstLogin);
+accountButtton.addEventListener('click', clickAccount);
+submitLoginButton.addEventListener('click', clickSubmitLogin);
+submitAccountButton.addEventListener('click', clickSubmitAccount);
+// enterButton.addEventListener('click', getStats);
