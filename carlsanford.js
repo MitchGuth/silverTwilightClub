4481871feeecc026c9329ccb6d28ca1f35f0d10f
SomeLinuxGuy <github@sect.net>
2018-08-30 15:51:30 -0400

    carl pseudo code

--- carlsanford.js
+++ carlsanford.js
@@ -1,1 +1,18 @@
-let myWill = "Done";
+// let myWill = "Done";
+
+// process money action 1
+// 	for each company in st_company:
+	
+// 	evaluate the winner:
+// 		select all bids where company and max bid
+// 		update his money stat to remove bid
+// 		company has a "chance" value
+// 		roll a d100:
+// 			if the roll is <= chance, you made money
+// 			add money (bid * 1.theroll) & add news
+// 			else the roll is > chance, you lost money
+// 			remove (bid * 1.theroll) & add news
+
+// 	evaluate losers:
+// 		select all bids where company and min bid
+// 		remove ( bid *.25 ) : remove 1 power & news
